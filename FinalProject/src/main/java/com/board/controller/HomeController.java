0a93062df8d262c41242e722fad6d663b4f3a1aa
verynahce <kims1577@naver.com>
2024-12-27 09:15:22 +0900

    redis 수정

--- FinalProject/src/main/java/com/board/controller/HomeController.java
+++ FinalProject/src/main/java/com/board/controller/HomeController.java
@@ -1,24 +1,16 @@
 package com.board.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
 
-	@Autowired
-    private RedisTemplate<String, String> redisTemplate;
+
 	// http://localhost:9090
 	@RequestMapping("/")
 	public  String   home() {
 		
-		return "home";
 
 		return "users/popup/info";
 		//return "/WEB-INF/views/home.jsp";
