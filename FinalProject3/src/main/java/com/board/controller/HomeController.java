fa6ab38c32328f6e128f642c1a7027f526872428
verynahce <kims1577@naver.com>
2025-01-13 15:00:47 +0900

    test 1

---
+++ FinalProject3/src/main/java/com/board/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	// http://localhost:9090
+	@RequestMapping("/")
+	public  String   home() {
+		return "management/list";
+		//return "/WEB-INF/views/home.jsp";
+	}
+
+	
+}
